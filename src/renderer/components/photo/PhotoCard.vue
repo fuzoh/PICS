d830ddd8484dcc5681258dbd1a01c68145f74d0a
jylecpnv <31761075+jylecpnv@users.noreply.github.com>
2017-11-16 16:41:03 +0100

    Overlay pictures
    - Add overlay for pictures that shows the name.

--- src/renderer/components/photo/PhotoCard.vue
+++ src/renderer/components/photo/PhotoCard.vue
@@ -5,6 +5,9 @@
   <div id="photo-card">
     <div class="photo-box">
       <img :src="url">
+      <div class="overlay" @click="displayDetails">
+          <div class="text">{{name}}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,6 +19,11 @@ export default {
   props: {
     url: "",
     name: ""
+  },
+  methods: {
+    displayDetails () {
+      this.$router.push('/picsDetails')
+    }
   }
 }
 
@@ -34,14 +42,55 @@ export default {
     box-sizing: border-box;
     width: 100%;
     height: 120px;
-
     overflow: hidden;
+    position: relative;
 
     img {
-      box-sizing: border-box;
-      width: 100%;
+    box-sizing: border-box;
+    width: 100%;
+    display:table-cell;
+    position: absolute;
+    margin: auto;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
     }
   }
+}
+
+.image {
+    display: block;
+    width: 100%;
+    height: auto;
+}
+
+.overlay {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0,0,0,0.5);
+    overflow: hidden;
+    width: 100%;
+    height: 0;
+    transition: .5s ease;
+}
+
+.photo-box:hover .overlay {
+    height: 100%;
+}
+
+.text {
+    white-space: nowrap;
+    color: white;
+    font-size: 20px;
+    position: absolute;
+    overflow: hidden;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
 }
 
 </style>
---
+++ src/renderer/components/photo/PicsDetails.vue
@@ -0,0 +1,25 @@
+<!-- PicsDetails.vue -->
+<!-- Displays the aside on the home wiew (contains the three wiewß) -->
+
+<template>
+  <div id="picsDetails">
+    <h1>Tutu</h1>
+    <img src="C:\Users\Jean-Yves.LE\Pictures\pourPics\Tutu\Penguins.jpg" alt="">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "picsDetails"
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+#picsDetails {
+
+}
+
+</style>
